8c29c3d52ae744c18761ca4d97e76b15298590a7
Ayush Saha <searchme.ayush@gmail.com>
2023-05-24 21:45:41 +0530

    made changes on the mobile screen

--- src/page/Mobile.jsx
+++ src/page/Mobile.jsx
@@ -3,8 +3,11 @@ import React from "react";
 function Mobile() {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center bg-yellow-400 sm:hidden">
-      <h1 className="my-auto">
-        <span className="font-medium">BitBasket</span> is a web only app
+      <h1 className="my-auto text-center">
+        <span className="block">
+          <span className="font-medium">BitBasket</span> is a web only app
+        </span>
+        <span className="block">Please view it on a PC or a Laptop</span>
       </h1>
       <div className="py-8 text-xs tracking-wide">
         Made by Ayush, Debarshi and Swarnali
